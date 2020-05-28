75a51b71f89e647c8bd8528b6c1a01196139ea87
Mingda Zheng <u6686733@anu.edu.au>
2020-05-29 06:23:01 +1000

    commit

---
+++ app/src/androidTest/java/com/example/voting/DBUtilsTest.java
@@ -0,0 +1,55 @@
+package com.example.voting;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DBUtilsTest {
+
+    private static boolean flag ;
+    private static boolean submitFlag;
+    private static ArrayList<String> information;
+    @BeforeClass
+    public static void arrange(){
+        flag = false;
+        submitFlag = true;
+        information = new ArrayList<>();
+        information.add("20");
+        information.add("test@gmail.com");
+        information.add("male");
+    }
+
+    @Test
+    public void loginJudge() {
+        assertEquals(flag,DBUtils.loginJudge("test","12345"));
+    }
+
+    @Test
+    public void register(){
+        boolean result = DBUtils.register("test","123456");
+        assertEquals(flag,result);
+    }
+
+
+    @Test
+    public void submitVote(){
+        ArrayList<String> submit = new ArrayList<>();
+        submit.add("What's your opinion?");
+        submit.add("choice1");
+        submit.add("choice2");
+        submit.add("05/07/2020");
+        boolean result = DBUtils.submitVote(submit);
+        assertEquals(submitFlag,result);
+    }
+
+    @Test
+    public void getInformation(){
+        ArrayList getInfo = DBUtils.getInformation("test");
+        boolean result;
+        result = getInfo.equals(information);
+        assertTrue("pass",result);
+    }
+}
