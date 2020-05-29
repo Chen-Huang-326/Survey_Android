890c05b745792eefbc9dc255c44f85d5f96c72ac
Zijun Zhang <u6904534@anu.edu.au>
2020-05-29 18:53:56 +1000

    fix merge problem in findsurvey

--- app/src/main/java/com/example/voting/FindSurvey.java
+++ app/src/main/java/com/example/voting/FindSurvey.java
@@ -49,7 +49,10 @@ public class FindSurvey extends AppCompatActivity {
         recyclerView.setAdapter(my_adapter);
         //recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
     }
-
+    public void clickFindSurveyBack(View v){
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
+    }
     private ArrayList<String[]> getData(){
         /*
         ArrayList<String[]> survey = DBUtils.getSurveyInfo();
